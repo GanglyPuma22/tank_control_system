8dd1c39101c3224a57bc555817e05a778b002391
Maxim Mounier <19mmounier1@gmail.com>
2025-09-02 23:46:03 -0700

    Cleaning up webpage code into modules and making page more functional

---
+++ src/public/js/config.js
@@ -0,0 +1,10 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyDaXSoK-VMwnAj5X0e_FOG826ABBagVfLE",
+  authDomain: "yasha-tank-controller.firebaseapp.com",
+  databaseURL: "https://yasha-tank-controller-default-rtdb.firebaseio.com",
+  projectId: "yasha-tank-controller",
+  storageBucket: "yasha-tank-controller.firebasestorage.app",
+  messagingSenderId: "446410548277",
+  appId: "1:446410548277:web:3549ce197d78fc97e47d7c",
+  measurementId: "G-37830TEG5L"
+};
---
+++ src/public/js/main.js
@@ -0,0 +1,31 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-app.js";
+import { getDatabase } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-database.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-auth.js";
+import { firebaseConfig } from './config.js';
+import { initializeAuth, login, logout } from './auth.js';
+import { initializeDevices, updateHeatLamp, updateLights, updateCamera } from './devices.js';
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const db = getDatabase(app);
+
+// Initialize modules
+initializeAuth(auth);
+initializeDevices(db);
+
+// Expose necessary functions to window for HTML event handlers
+globalThis.login = login;
+globalThis.logout = logout;
+globalThis.updateHeatLamp = updateHeatLamp;
+globalThis.updateLights = updateLights;
+globalThis.updateCamera = updateCamera;
+
+// Also expose on window for backwards compatibility
+Object.assign(window, {
+    login,
+    logout,
+    updateHeatLamp,
+    updateLights,
+    updateCamera
+});
