7ccd65807e5fd0aa2ce03f75485f39797f0162a9
Maxim Mounier <19mmounier1@gmail.com>
2025-09-10 00:19:07 -0700

    Adding OTA update support to WIFI init
    Great for when you setup the system but still want to push code updates!

--- src/utils/WiFiUtil.h
+++ src/utils/WiFiUtil.h
@@ -1,5 +1,6 @@
 #pragma once
 #include "../config/Credentials.h" // Put WIFI_SSID and WIFI_PASSWORD here (git-ignored)
+#include <ArduinoOTA.h>
 #include <WiFi.h>
 #include <time.h>
 
@@ -11,7 +12,32 @@ constexpr const char *ntpServer = "pool.ntp.org";
 // Pacific Time with automatic DST rules
 constexpr const char *tzInfo = "PST8PDT,M3.2.0/2,M11.1.0/2";
 
-// ----- CONNECT + NTP SYNC -----
+inline void setupOTA() {
+  // OTA setup
+  ArduinoOTA.onStart([]() { Serial.println("OTA Update Start"); })
+      .onEnd([]() { Serial.println("OTA Update End"); })
+      .onProgress([](unsigned int progress, unsigned int total) {
+        Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
+      })
+      .onError([](ota_error_t error) {
+        Serial.printf("Error[%u]: ", error);
+        if (error == OTA_AUTH_ERROR)
+          Serial.println("Auth Failed");
+        else if (error == OTA_BEGIN_ERROR)
+          Serial.println("Begin Failed");
+        else if (error == OTA_CONNECT_ERROR)
+          Serial.println("Connect Failed");
+        else if (error == OTA_RECEIVE_ERROR)
+          Serial.println("Receive Failed");
+        else if (error == OTA_END_ERROR)
+          Serial.println("End Failed");
+      });
+
+  ArduinoOTA.begin();
+  Serial.println("OTA ready. IP address: " + WiFi.localIP().toString());
+}
+
+// ----- CONNECT + OTA + NTP SYNC -----
 inline void connectAndSyncTime() {
   Serial.print("Connecting to ");
   Serial.println(WIFI_SSID);
@@ -35,6 +61,9 @@ inline void connectAndSyncTime() {
   Serial.print("MAC Address: ");
   Serial.println(WiFi.macAddress());
 
+  // Setup OTA - for wireless updates
+  setupOTA();
+
   // Set timezone and start NTP sync
   configTzTime(tzInfo, ntpServer);
 
@@ -56,6 +85,7 @@ inline void maintain() {
     WiFi.disconnect();
     WiFi.reconnect();
   }
+  ArduinoOTA.handle(); // Keep checking for OTA updates
 }
 
 // ----- GET LOCAL TIME -----
